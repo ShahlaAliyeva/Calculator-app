60e27412ff00807e7b39840a4f82befd9aad20f7
ShahlaAliyeva <raufa5740@gmail.com>
2023-01-20 17:18:14 +0400

    math functions are added

--- assets/js/index.js
+++ assets/js/index.js
@@ -80,141 +80,142 @@ function getLightTheme() {
 }
 
 function getDarkTheme() {
+  $(gradientThemeButton).removeClass('" visible-gradient "');
+  $(lightThemeButton).removeClass('" visible-light "');
 
-    $(gradientThemeButton).removeClass('" visible-gradient "');
-    $(lightThemeButton).removeClass('" visible-light "');
-  
-    $("body").css("background-color", "hsl(222, 26%, 31%)");
-    $(navbar).css("color", "hsl(0, 0%, 100%)");
-    $(".count-buttons").css("background-color", "hsl(223, 31%, 20%)");
-    $(".count-buttons button").css("background-color", "hsl(223, 31%, 20%)");
-    $(".visible-dark").css("background-color", "hsl(6, 63%, 50%)");
-  
-    $("#calc-monitor").css("background-color", "hsl(224, 36%, 15%)");
-    $("#calc-monitor p").css("color", "hsl(0, 0%, 100%)");
-    $("#calc-monitor").css("color", "hsl(0, 0%, 100%)");
-  
-    $("#keypad-section").css("background-color", "hsl(223, 31%, 20%)");
-    $("#keypad-section .calc-buttons div").css(
-      "background-color",
-      "hsl(30, 25%, 89%)"
-    );
-    $("#keypad-section .calc-buttons div").css(
-      "box-shadow",
-      "0px -5px 4px hsl(28, 16%, 65%) inset"
-    );
-    $("#keypad-section .calc-buttons div").css("color", "hsl(221, 14%, 31%)");
-  
-    $("#keypad-section .calc-buttons .delete-btn").css(
-      "background-color",
-      "hsl(225, 21%, 49%)"
-    );
-    $("#keypad-section .calc-buttons .reset-btn").css(
-      "background-color",
-      "hsl(225, 21%, 49%)"
-    );
-    $("#keypad-section .calc-buttons .delete-btn").css(
-      "box-shadow",
-      "0px -5px 4px hsl(224, 28%, 35%) inset"
-    );
-    $("#keypad-section .calc-buttons .reset-btn").css(
-      "box-shadow",
-      "0px -5px 4px hsl(224, 28%, 35%) inset"
-    );
-    $("#keypad-section .calc-buttons .delete-btn").css(
-      "color",
-      "hsl(0, 0%, 100%)"
-    );
-    $("#keypad-section .calc-buttons .reset-btn").css(
-      "color",
-      "hsl(0, 0%, 100%)"
-    );
-  
-    $("#keypad-section .calc-buttons .equal-btn").css(
-      "background-color",
-      "hsl(6, 63%, 50%)"
-    );
-    $("#keypad-section .calc-buttons .equal-btn").css(
-      "box-shadow",
-      "0px -5px 4px hsl(6, 70%, 34%) inset"
-    );
-    $("#keypad-section .calc-buttons .equal-btn").css(
-      "color",
-      "hsl(0, 0%, 100%)"
-    );
+  $("body").css("background-color", "hsl(222, 26%, 31%)");
+  $(navbar).css("color", "hsl(0, 0%, 100%)");
+  $(".count-buttons").css("background-color", "hsl(223, 31%, 20%)");
+  $(".count-buttons button").css("background-color", "hsl(223, 31%, 20%)");
+  $(".visible-dark").css("background-color", "hsl(6, 63%, 50%)");
+
+  $("#calc-monitor").css("background-color", "hsl(224, 36%, 15%)");
+  $("#calc-monitor p").css("color", "hsl(0, 0%, 100%)");
+  $("#calc-monitor").css("color", "hsl(0, 0%, 100%)");
+
+  $("#keypad-section").css("background-color", "hsl(223, 31%, 20%)");
+  $("#keypad-section .calc-buttons div").css(
+    "background-color",
+    "hsl(30, 25%, 89%)"
+  );
+  $("#keypad-section .calc-buttons div").css(
+    "box-shadow",
+    "0px -5px 4px hsl(28, 16%, 65%) inset"
+  );
+  $("#keypad-section .calc-buttons div").css("color", "hsl(221, 14%, 31%)");
+
+  $("#keypad-section .calc-buttons .delete-btn").css(
+    "background-color",
+    "hsl(225, 21%, 49%)"
+  );
+  $("#keypad-section .calc-buttons .reset-btn").css(
+    "background-color",
+    "hsl(225, 21%, 49%)"
+  );
+  $("#keypad-section .calc-buttons .delete-btn").css(
+    "box-shadow",
+    "0px -5px 4px hsl(224, 28%, 35%) inset"
+  );
+  $("#keypad-section .calc-buttons .reset-btn").css(
+    "box-shadow",
+    "0px -5px 4px hsl(224, 28%, 35%) inset"
+  );
+  $("#keypad-section .calc-buttons .delete-btn").css(
+    "color",
+    "hsl(0, 0%, 100%)"
+  );
+  $("#keypad-section .calc-buttons .reset-btn").css(
+    "color",
+    "hsl(0, 0%, 100%)"
+  );
+
+  $("#keypad-section .calc-buttons .equal-btn").css(
+    "background-color",
+    "hsl(6, 63%, 50%)"
+  );
+  $("#keypad-section .calc-buttons .equal-btn").css(
+    "box-shadow",
+    "0px -5px 4px hsl(6, 70%, 34%) inset"
+  );
+  $("#keypad-section .calc-buttons .equal-btn").css(
+    "color",
+    "hsl(0, 0%, 100%)"
+  );
 }
 
 function getGradientTheme() {
-    $(darkThemeButton).removeClass('" visible-dark "');
-    $(lightThemeButton).removeClass('" visible-light "');
+  $(darkThemeButton).removeClass('" visible-dark "');
+  $(lightThemeButton).removeClass('" visible-light "');
 
-    $("body").css("background-color", "hsl(268, 75%, 9%)");
-    $(navbar).css("color", "hsl(52, 100%, 62%)");
-    $(".count-buttons").css("background-color", "hsl(268, 71%, 12%)");
-    $(".count-buttons button").css("background-color", "hsl(268, 71%, 12%)");
-    $(".visible-gradient").css("background-color", "hsl(176, 100%, 44%)");
-  
-    $("#calc-monitor").css("background-color", "hsl(268, 71%, 12%)");
-    $("#calc-monitor p").css("color", "hsl(52, 100%, 62%)");
-    $("#calc-monitor").css("color", "hsl(52, 100%, 62%)");
-  
-    $("#keypad-section").css("background-color", "hsl(268, 71%, 12%)");
-    $("#keypad-section .calc-buttons div").css(
-      "background-color",
-      "hsl(268, 47%, 21%)"
-    );
-    $("#keypad-section .calc-buttons div").css(
-      "box-shadow",
-      "0px -5px 4px hsl(290, 70%, 36%) inset"
-    );
-    $("#keypad-section .calc-buttons div").css("color", "hsl(52, 100%, 62%)");
-  
-    $("#keypad-section .calc-buttons .delete-btn").css(
-      "background-color",
-      "hsl(281, 89%, 26%)"
-    );
-    $("#keypad-section .calc-buttons .reset-btn").css(
-      "background-color",
-      "hsl(281, 89%, 26%)"
-    );
-    $("#keypad-section .calc-buttons .delete-btn").css(
-      "box-shadow",
-      "0px -5px 4px hsl(285, 91%, 52%) inset"
-    );
-    $("#keypad-section .calc-buttons .reset-btn").css(
-      "box-shadow",
-      "0px -5px 4px hsl(285, 91%, 52%) inset"
-    );
-    $("#keypad-section .calc-buttons .delete-btn").css(
-      "color",
-      "hsl(0, 0%, 100%)"
-    );
-    $("#keypad-section .calc-buttons .reset-btn").css(
-      "color",
-      "hsl(0, 0%, 100%)"
-    );
-  
-    $("#keypad-section .calc-buttons .equal-btn").css(
-      "background-color",
-      "hsl(176, 100%, 44%)"
-    );
-    $("#keypad-section .calc-buttons .equal-btn").css(
-      "box-shadow",
-      "0px -5px 4px hsl(177, 92%, 70%) inset"
-    );
-    $("#keypad-section .calc-buttons .equal-btn").css(
-      "color",
-      "hsl(198, 20%, 13%)"
-    );
+  $("body").css("background-color", "hsl(268, 75%, 9%)");
+  $(navbar).css("color", "hsl(52, 100%, 62%)");
+  $(".count-buttons").css("background-color", "hsl(268, 71%, 12%)");
+  $(".count-buttons button").css("background-color", "hsl(268, 71%, 12%)");
+  $(".visible-gradient").css("background-color", "hsl(176, 100%, 44%)");
+
+  $("#calc-monitor").css("background-color", "hsl(268, 71%, 12%)");
+  $("#calc-monitor p").css("color", "hsl(52, 100%, 62%)");
+  $("#calc-monitor").css("color", "hsl(52, 100%, 62%)");
+
+  $("#keypad-section").css("background-color", "hsl(268, 71%, 12%)");
+  $("#keypad-section .calc-buttons div").css(
+    "background-color",
+    "hsl(268, 47%, 21%)"
+  );
+  $("#keypad-section .calc-buttons div").css(
+    "box-shadow",
+    "0px -5px 4px hsl(290, 70%, 36%) inset"
+  );
+  $("#keypad-section .calc-buttons div").css("color", "hsl(52, 100%, 62%)");
+
+  $("#keypad-section .calc-buttons .delete-btn").css(
+    "background-color",
+    "hsl(281, 89%, 26%)"
+  );
+  $("#keypad-section .calc-buttons .reset-btn").css(
+    "background-color",
+    "hsl(281, 89%, 26%)"
+  );
+  $("#keypad-section .calc-buttons .delete-btn").css(
+    "box-shadow",
+    "0px -5px 4px hsl(285, 91%, 52%) inset"
+  );
+  $("#keypad-section .calc-buttons .reset-btn").css(
+    "box-shadow",
+    "0px -5px 4px hsl(285, 91%, 52%) inset"
+  );
+  $("#keypad-section .calc-buttons .delete-btn").css(
+    "color",
+    "hsl(0, 0%, 100%)"
+  );
+  $("#keypad-section .calc-buttons .reset-btn").css(
+    "color",
+    "hsl(0, 0%, 100%)"
+  );
+
+  $("#keypad-section .calc-buttons .equal-btn").css(
+    "background-color",
+    "hsl(176, 100%, 44%)"
+  );
+  $("#keypad-section .calc-buttons .equal-btn").css(
+    "box-shadow",
+    "0px -5px 4px hsl(177, 92%, 70%) inset"
+  );
+  $("#keypad-section .calc-buttons .equal-btn").css(
+    "color",
+    "hsl(198, 20%, 13%)"
+  );
 }
 
 $(document).ready(function () {
+  getDarkTheme();
+
   $(darkThemeButton).click(function () {
     if (!$(darkThemeButton).hasClass("visible-dark")) {
       $(darkThemeButton).addClass('" visible-dark "');
-      //Dark theme CSS
-      getDarkTheme()
 
+      //Dark theme CSS
+      getDarkTheme();
     }
   });
 
@@ -232,27 +233,21 @@ $(document).ready(function () {
       $(gradientThemeButton).addClass('" visible-gradient "');
 
       //Gradient theme CSS
-      getGradientTheme()
+      getGradientTheme();
     }
   });
-
-  // calcKeys.forEach((element, index) => {
-  //     $(buttons).click(function () {
-  //         if(keyArr[index] == element.textContent) {
-  //             console.log(buttons[index])
-  //         }
-  //     });
-  // });
 
   let number = "";
   let secondNumber = "";
   let num1 = "";
   let num2 = "";
   let operator = "";
+  let result = "";
+  let operationCount = 0;
   $(".calc-buttons div").on("click", function () {
     let key = $(this).valueOf()[0].innerText;
-    let result = 0;
-    // console.log('Key: ', key)
+
+    console.log("Key: ", key);
 
     if (
       key !== "DEL" &&
@@ -265,7 +260,6 @@ $(document).ready(function () {
     ) {
       $(".result").remove();
       if (operator !== "") {
-        number = "";
         console.log("operator: ", operator);
         number = number + key;
         num2 = number;
@@ -280,56 +274,88 @@ $(document).ready(function () {
     } else if (key === "DEL") {
       // number -= number.charAt(number.length - 1)
       number = number.substring(0, number.length - 1);
-      num1 = number;
-      $(".num1").text(num1);
-      console.log(num1);
+      // if(number.length === 0) {
+      //   operator =''
+      //   $('.operator').text(operator)
+      // } else
+
+      // operator num1 den evvel silinende num1 silinmir indi hal-hazirda da num1 operatordan evvel silinir operator yerinde qalir sonra da alem deyir
+      
+      if (num1 !== "" && num2 !== "") {
+
+        if(number.length === 0) {
+          num2 =''
+        } else {
+          num2 = number;
+        }
+        $(".num2").text(num2);
+        console.log("Deleted number 2: ", num2);
+      } else if (result !== "") {
+        result = result.substring(0, result.length - 1);
+        console.log("Deleted result num: ", result);
+        $(".result").text(result);
+      } else {
+        if(number.length === 0) {
+          num1 =''
+        } else {
+          num1 = number;
+        }
+        $(".num1").text(num1);
+        console.log(num1);
+      }
     } else if (key === "RESET") {
       number = "";
       num1 = number;
       num2 = number;
       operator = "";
+      result = "";
       $(".num1").text(num1);
       $(".num2").text(num2);
       $(".operator").text(operator);
+      $(".result").text(result);
     } else if (key === "-" || key === "+" || key === "/" || key === "X") {
-      num1 = number;
+      if (result !== "") {
+        number = result;
+        num1 = number;
+        result = "";
+
+        $(".num1").text(num1);
+        $(".result").remove();
+        console.log("result is num1 now: ", num1);
+      }
+
       if (num2 === "") {
+        num1 = number;
+        console.log("num1: ", num1);
         operator = $(this).valueOf()[0].innerText;
         $(".operator").text(operator);
         console.log("operator: ", operator);
-
-        console.log("num1: ", num1);
+        //ilk if scope undan cixardildi
+        number = "";
+      } else if (num1!=='' && num2!=='') {
+        operator = $(this).valueOf()[0].innerText;
+        $(".operator").text(operator);
+        console.log("operator: ", operator);
       }
 
-      // if (operator !== '') {
-      //     console.log('operator: ', operator)
-      //     num1 = number
-      //     number = ''
-      //     if(key !== 'DEL'  && key !== '-' && key !== '+' && key !== '/' && key !== 'X' && key !== 'RESET' && key !== '='){
-      //         number = number+key
-      //         num2 = number
-      //         $( '.num2' ).text(num2);
-      //         console.log('number 2: ', num2)
-      //     }
-      // }
     } else if (key === "=") {
       if (num1 !== "" && num2 !== "") {
         if (operator === "+") {
           let sum = 0;
           sum = parseFloat(num1) + parseFloat(num2);
-          result = sum;
+          result = sum.toString();
         } else if (operator === "-") {
           let difference = 0;
           difference = parseFloat(num1) - parseFloat(num2);
-          result = difference;
+          result = difference.toString();
         } else if (operator === "/") {
           let division = 1;
           division = parseFloat(num1) / parseFloat(num2);
-          result = division;
+          result = division.toString();
         } else if (operator === "X") {
           let multiple = 1;
           multiple = parseFloat(num1) * parseFloat(num2);
-          result = multiple;
+          result = multiple.toString();
         }
 
         console.log("result", result);
@@ -344,7 +370,5 @@ $(document).ready(function () {
       $(".operator").text(operator);
       $(`<p class="result light-th">${result}</p>`).appendTo("#calc-monitor");
     }
-    // delete de problem var
-    //    console.log( ($(this).valueOf())[0].innerText);
   });
 });
